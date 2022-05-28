ad43a36f8ffda86ee044f01c0d8b93979de67923
James LaFritz <ktmarine1999@gmail.com>
2022-05-28 15:57:48 -0400

    Created a View for the Behavior Tree Node.

--- Editor/BehaviorTreeNodeView.cs
+++ Editor/BehaviorTreeNodeView.cs
@@ -2,28 +2,20 @@
 // 05-15-2022
 // James LaFritz
 
-using UnityEditor;
-using UnityEditor.UIElements;
-using UnityEngine.UIElements;
-
 namespace GraphViewBehaviorTree.Editor
 {
+    /// <summary>
+    /// A View for Behavior Tree Nodes, based on the Graph View Node
+    /// </summary>
     public class BehaviorTreeNodeView : UnityEditor.Experimental.GraphView.Node
     {
-        Node m_node;
+        private Node m_node;
 
         public BehaviorTreeNodeView(Node node)
         {
             m_node = node;
-            base.title = node.name;
-
-            SerializedObject so = new SerializedObject(m_node);
-
-            VisualTreeAsset visualTree =
-                AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(
-                    "Assets/GraphViewBehaviorTree/Editor/BehaviorTreeEditor.uxml");
-            visualTree.CloneTree(this);
-            this.Bind(so);
+            if (m_node == null) return;
+            base.title = m_node.GetType().Name;
         }
     }
 }
