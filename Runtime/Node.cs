90059bc9ae0bf9ece7ea464cff834868476671af
James LaFritz <ktmarine1999@gmail.com>
2022-05-28 14:29:40 -0400

    Made Node a normal class

--- Runtime/Node.cs
+++ Runtime/Node.cs
@@ -5,7 +5,8 @@ namespace GraphViewBehaviorTree
     /// <summary>
     /// Base class for all nodes in the Behavior tree.
     /// </summary>
-    public abstract class Node : Object
+    [System.Serializable]
+    public abstract class Node
     {
         /// <summary>
         /// The states a node can be in.
@@ -32,7 +33,7 @@ namespace GraphViewBehaviorTree
 
         private bool m_started;
 
-        public int nodeID;
+        public string guid;
 
         /// <summary>
         /// Runs when the Node first starts running.
