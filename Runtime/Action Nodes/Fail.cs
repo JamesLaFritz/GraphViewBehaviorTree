e1972d799447b0f60c300d51b4c9a9f026f739a9
James LaFritz <ktmarine1999@gmail.com>
2022-06-01 00:11:00 -0400

    Added Fail Node

---
+++ Runtime/Action Nodes/Fail.cs
@@ -0,0 +1,25 @@
+// Fail.cs
+// 06-01-2022
+// James LaFritz
+
+namespace GraphViewBehaviorTree
+{
+    public class Fail : ActionNode
+    {
+        #region Overrides of Node
+
+        /// <inheritdoc />
+        protected override void OnStart() { }
+
+        /// <inheritdoc />
+        protected override void OnStop() { }
+
+        /// <inheritdoc />
+        protected override State OnUpdate()
+        {
+            return State.Failure;
+        }
+
+        #endregion
+    }
+}
